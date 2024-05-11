56618795da7bad02b9c53a73a98089240bcc43a9
Youssef Alashiq <y.alashiq11@gmail.com>
2024-05-11 21:29:05 +0300

    code on PC

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ app.use(express.json());
 
 const members = JSON.parse(fs.readFileSync('./member.json'));
 const trainers = JSON.parse(fs.readFileSync('./trainer.json'));
+const newArray = JSON.parse(JSON.stringify(trainers));
 
+
+app.route('/members').get()
 
 app.get('/members', (req, res) => {
-  res.send(members);
+  res.json(members);
 });
 
 app.get('/members/:id', (req, res) => {
@@ -21,14 +24,50 @@ app.get('/members/:id', (req, res) => {
 
   if (result === undefined) {
 
-    res.send("sorry this member not here")
+    res.send("sorry member not found")
   } else if (result.status == "freeze") {
     res.send("this member is not allowed to enter the gym")
+
+  } else if (result.deleted == true) {
+    res.send("sorry 'member not found");
 
   } else {
     res.json(result);
 
   }
+});
+app.get('/trainers/:id', (req, res) => {
+
+  const id = req.params.id * 1;
+
+  const result = trainers.findIndex(el => el.id === id);
+
+  if (result == -1) {
+    res.status(404).json({
+      message: "sorry member not found"
+    });
+
+  } else {
+
+    newArray[result].memberID = [];
+
+    for (let j = result; j < members.length; j++) {
+
+      if (trainers[result].id === members[j].trainerID) {
+        newArray[result].memberID.push(members[j].id);
+
+      }
+    }
+
+    res.json(newArray[result])
+
+
+
+  }
+
+
+
+
 });
 
 
@@ -43,41 +82,132 @@ app.patch('/members/:id', (req, res) => {
   if (result === undefined) {
 
     res.status(404).json({
-      message: "sorry this member not here"
+      message: "sorry member not found"
     })
   } else {
 
-    const name = req.body.name;
-    const fromMembership = req.body.memberShip.from;
-    const toMembership = req.body.memberShip.to;
-    const cost = req.body.memberShip.cost;
-    const trinerID = req.body.trinerID;
-
+    const {
+      name,
+      trainerID
+    } = req.body;
+    const {
+      from,
+      to,
+      cost
+    } = req.body.memberShip
 
     result.name = name;
-    result.memberShip.from = fromMembership;
-    result.memberShip.to = toMembership;
+    result.memberShip.from = from;
+    result.memberShip.to = to;
     result.memberShip.cost = cost;
-    result.trinerID = trinerID;
+    result.trainerID = trainerID;
 
     fs.writeFileSync('member.json', JSON.stringify(members));
     res.status(201).json({
       message: "member updated succefully"
     });
 
+  }
+
+})
+app.patch('/trainers/:id', (req, res) => {
+
+  const id = req.params.id * 1;
+  const result = trainers.find(el => el.id === id);
 
 
+  if (result === undefined) {
+
+    res.status(404).json({
+      message: "sorry trainer not found"
+    })
+  } else {
+
+    const {
+      name
+    } = req.body;
+
+    const {
+      from,
+      to
+    } = req.body.duration
+
+    result.name = name;
+    result.duration.from = from;
+    result.duration.to = to;
+
+
+    fs.writeFileSync('trainer.json', JSON.stringify(trainers));
+    res.status(201).json({
+      message: "trainer updated succefully"
+    });
 
   }
 
-
-
 })
+
+app.post('/members', (req, res) => {
+  members.push(req.body);
+  fs.writeFileSync('member.json', JSON.stringify(members));
+  res.send("user added");
+
+});
+
+app.delete('/members/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const index = members.findIndex(item => item.id === id);
+  if (index !== -1) {
+    members[index].deleted = true;
+    fs.writeFileSync('./member.json', JSON.stringify(members));
+    res.json({
+      message: 'member soft deleted'
+    });
+
+  } else {
+    res.status(404).json({
+      message: 'member not found'
+    });
+  }
+});
+
+app.delete('/trainers/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const index = trainers.findIndex(item => item.id === id);
+  if (index !== -1) {
+    trainers.splice(index, 1);
+    fs.writeFileSync('./trainer.json', JSON.stringify(trainers));
+    res.json({
+      message: 'trainer  deleted'
+    });
+
+  } else {
+    res.status(404).json({
+      message: 'trainer not found'
+    });
+  }
+});
+
 
 app.get('/trainers', (req, res) => {
 
-  res.send(trainers);
+
+  for (let i = 0; i < trainers.length; i++) {
+    newArray[i].memberID = [];
+
+
+    for (let j = 0; j < members.length; j++) {
+
+      if (trainers[i].id === members[j].trainerID) {
+        newArray[i].memberID.push(members[j].id);
+
+      }
+    }
+  }
+  res.end(JSON.stringify(newArray))
+
+
 })
+
 
 
 app.post('/trainers', (req, res) => {
@@ -88,12 +218,6 @@ app.post('/trainers', (req, res) => {
   res.send("trainer added")
 })
 
-app.post('/members', (req, res) => {
-  members.push(req.body);
-  fs.writeFileSync('member.json', JSON.stringify(members));
-  res.send("user added");
-
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
