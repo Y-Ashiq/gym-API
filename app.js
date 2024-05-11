8dde66fcbab10324fdf5e79a9cce65d60ceb95e9
Youssef Alashiq <y.alashiq11@gmail.com>
2024-05-11 14:26:26 +0300

    new

--- app.js
+++ app.js
@@ -1,26 +1,99 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const fs = require("fs");
+const fs = require('fs');
 
 app.use(express.json());
 
 
-const members = JSON.parse(fs.readFileSync('./member.json' , 'utf-8'));
+const members = JSON.parse(fs.readFileSync('./member.json'));
+const trainers = JSON.parse(fs.readFileSync('./trainer.json'));
 
-app.get('/member', (req, res) => {
-  console.log(members);
-  res.status(200).send(members);
+
+app.get('/members', (req, res) => {
+  res.send(members);
 });
 
-app.post('/member', (req, res) => {
+app.get('/members/:id', (req, res) => {
 
-  const data = 5;
-  
-  members.push(data);
-  fs.writeFileSync('./member.json', JSON.stringify(data));
-  res.send("ok");
+  const id = req.params.id * 1;
+  const result = members.find(el => el.id === id);
+
+  if (result === undefined) {
+
+    res.send("sorry this member not here")
+  } else if (result.status == "freeze") {
+    res.send("this member is not allowed to enter the gym")
+
+  } else {
+    res.json(result);
+
+  }
+});
+
+
+
+
+app.patch('/members/:id', (req, res) => {
+
+  const id = req.params.id * 1;
+  const result = members.find(el => el.id === id);
+
+
+  if (result === undefined) {
+
+    res.status(404).json({
+      message: "sorry this member not here"
+    })
+  } else {
+
+    const name = req.body.name;
+    const fromMembership = req.body.memberShip.from;
+    const toMembership = req.body.memberShip.to;
+    const cost = req.body.memberShip.cost;
+    const trinerID = req.body.trinerID;
+
+
+    result.name = name;
+    result.memberShip.from = fromMembership;
+    result.memberShip.to = toMembership;
+    result.memberShip.cost = cost;
+    result.trinerID = trinerID;
+
+    fs.writeFileSync('member.json', JSON.stringify(members));
+    res.status(201).json({
+      message: "member updated succefully"
+    });
+
+
+
+
+  }
+
+
+
 })
+
+app.get('/trainers', (req, res) => {
+
+  res.send(trainers);
+})
+
+
+app.post('/trainers', (req, res) => {
+
+
+  trainers.push(req.body)
+  fs.writeFileSync('trainer.json', JSON.stringify(trainers));
+  res.send("trainer added")
+})
+
+app.post('/members', (req, res) => {
+  members.push(req.body);
+  fs.writeFileSync('member.json', JSON.stringify(members));
+  res.send("user added");
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
